047743da55b54aa1d262455102e43f935577268b
choiwj0706 <choiwj0706@gmail.com>
2025-01-06 09:55:01 +0900

    예상질문 저장 / 라우터 연결

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
-from app.routers import FaceRegistration, FaceImage, FaceLogin
+from app.routers import FaceRegistration, FaceImage, FaceLogin, AddQuestionNAnswer
 
 # FastAPI 인스턴스 생성
 app = FastAPI()
@@ -21,7 +21,7 @@ app.add_middleware(
 app.include_router(FaceRegistration.router, prefix="/faceRegistration", tags=["Face Registration"])
 app.include_router(FaceImage.router, prefix="/faceId", tags=["Face ID"])  # 새로운 라우터 등록
 app.include_router(FaceLogin.router, prefix="/faceLogin", tags=["Face Login"])  # 새로운 라우터 등록
-
+app.include_router(AddQuestionNAnswer.router, prefix="/interview", tags=["Interview Questions"])
 
 @app.get("/")
 def read_root():
