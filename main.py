eb477a9a1390b95c81910fca6d89c44a93989de2
Entity <168009904+SWCGoing@users.noreply.github.com>
2025-01-07 16:43:59 +0900

    Update AddselfIntroduce

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import StreamingResponse
 import uvicorn
 import cv2
-from app.routers import FaceRegistration, FaceImage, FaceLogin, AddQuestionNAnswer, AiInterview
+from app.routers import FaceRegistration, FaceImage, FaceLogin, AddQuestionNAnswer, AiInterview, AddSelfIntroduce
 
 # FastAPI 인스턴스 생성
 app = FastAPI()
@@ -12,6 +12,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:3000", "http://localhost:8080", "http://127.0.0.1:8080"],
+    allow_credentials=True,  # credentials 허용
     allow_methods=["*"],
     allow_headers=["*"],
 )
@@ -22,6 +23,7 @@ app.include_router(FaceImage.router, prefix="/faceId", tags=["Face ID"])
 app.include_router(FaceLogin.router, prefix="/faceLogin", tags=["Face Login"])
 app.include_router(AddQuestionNAnswer.router, prefix="/interview", tags=["Interview Questions"])
 app.include_router(AiInterview.router, prefix="/aiInterview", tags=["AI Interview"])  # AiInterview 라우터 추가
+app.include_router(AddSelfIntroduce.router, prefix="/addSelfIntroduce", tags=["Add Self Introduce"])  #
 
 
 @app.get("/")
