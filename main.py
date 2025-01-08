a53306e4af94491983ea85c95300e8fcd39915d7
Choi Man Kyeong <choimatthew0326@gmail.com>
2025-01-08 14:16:29 +0900

    AudioAnalyze.py , VideoAnalyze.py setting

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import StreamingResponse
 import uvicorn
 import cv2
-from app.routers import FaceRegistration, FaceImage, FaceLogin, AddQuestionNAnswer, AiInterview, AddSelfIntroduce, Voice, VideoPosition, InterviewSave
+from app.routers import FaceRegistration, FaceImage, FaceLogin, AddQuestionNAnswer, AiInterview, AddSelfIntroduce, Voice, VideoPosition, InterviewSave, VideoAnalyze, AudioAnalyze
 
 # FastAPI 인스턴스 생성
 app = FastAPI()
@@ -26,7 +26,10 @@ app.include_router(AiInterview.router, prefix="/aiInterview", tags=["AI Intervie
 app.include_router(AddSelfIntroduce.router, prefix="/addSelfIntroduce", tags=["Add Self Introduce"])
 app.include_router(Voice.router, prefix="/voice", tags=["Voice"])
 app.include_router(VideoPosition.router, prefix="/videoPosition", tags=["VideoPosition"])
-app.include_router(InterviewSave.router, prefix="/interviewSave", tags=["VideoPosition"])
+app.include_router(InterviewSave.router, prefix="/interviewSave", tags=["interviewSave"])
+app.include_router(VideoAnalyze.router, prefix="/videoAnalyze", tags=["videoAnalyze"])
+app.include_router(AudioAnalyze.router, prefix="/audioAnalyze", tags=["audioAnalyze"])
+
 @app.get("/")
 def read_root():
     return {"message": "Welcome to the JOBIS FastAPI!"}
